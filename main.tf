6cc54b19187530f5a3fc8df6187524de645b6050
ajanin2506-proj <ajanin2506@gmail.com>
2026-02-09 04:38:36 +0530

    Refactor Terraform configuration for S3 bucket

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
 terraform {
   required_providers {
-    variable "bucket_name" {
-    description = "Name of the S3 bucket"
-    type        = string
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
