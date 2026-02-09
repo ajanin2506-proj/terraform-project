8889ea6b74cbfc5760dea6089cba2cda5361a300
ajanin2506-proj <ajanin2506@gmail.com>
2026-02-10 04:49:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
 terraform {
   required_providers {
     aws = {
@@ -16,6 +11,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "ajan-terraform-learning-bucket-0110"
+  bucket = "ajan-terraform-learning-bucket-0320"
 }
