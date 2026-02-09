017be2d0660b295c1f94ff72ce45588bccb2e023
ajanin2506-proj <ajanin2506@gmail.com>
2026-02-10 04:53:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,5 +13,8 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "ajan-terraform-learning-bucket-0110"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
   bucket = "ajan-terraform-learning-bucket-0320"
 }
